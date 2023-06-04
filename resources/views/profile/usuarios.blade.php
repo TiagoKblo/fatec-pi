124e61b1c8c46fca0923b362538018f8d0d94dff
TiagoKblo <kblotiago@gmail.com>
2023-06-04 10:01:41 -0300

    modificacao formatacao

--- resources/views/profile/usuarios.blade.php
+++ resources/views/profile/usuarios.blade.php
@@ -11,50 +11,59 @@
                 <div class="p-6 text-gray-900">
                     <h1 class="font-bold text-3xl">Ações Administrativas</h1>
                     <ul class="mt-2">
-                        <li class="list-disc ml-5"><a href="{{ route('gerenciar') }}" class="underline text-blue-500">Gerenciar Editais</a></li>
-                        <li class="list-disc ml-5"><a href="{{ route('manifestar.index') }}" class="underline text-blue-500">Gerenciar Manifestações<a></li>
-                        <li class="list-disc ml-5"><a href="{{ route('profile.usuarios') }}" class="underline text-blue-500">Gerenciar Usuários</a></li>
+                        <li class="list-disc ml-5"><a href="{{ route('gerenciar') }}"
+                                class="underline text-blue-500">Gerenciar Editais</a></li>
+                        <li class="list-disc ml-5"><a href="{{ route('manifestar.index') }}"
+                                class="underline text-blue-500">Gerenciar Manifestações<a></li>
+                        <li class="list-disc ml-5"><a href="{{ route('profile.usuarios') }}"
+                                class="underline text-blue-500">Gerenciar Usuários</a></li>
                     </ul>
+                </div>
+
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <section>
+                        <header>
+                            <h2 class="text-lg font-medium text-gray-900">
+                                {{ __('Gerenciar Usuários') }}
+                            </h2>
+                        </header>
+
+
+                        <div class="mt-6">
+                            <table class="min-w-full">
+                                <thead>
+                                    <tr>
+                                        <th class="px-4 py-2">{{ __('Nome') }}</th>
+                                        <th class="px-4 py-2">{{ __('E-mail') }}</th>
+                                        <th class="px-4 py-2">{{ __('Unidade') }}</th>
+                                        <th class="px-4 py-2">{{ __('Ações') }}</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($usuarios as $user)
+                                        <tr>
+                                            <td class="border px-4 py-2">{{ $user->name }}</td>
+                                            <td class="border px-4 py-2">{{ $user->email }}</td>
+                                            <td class="border px-4 py-2">
+                                                {{ $user->unidade ?? 'Fatec Itapira - Ogari de Castro Pacheco' }}</td>
+                                            <td class="border px-4 py-2">
+                                                <a href="{{ route('profile.edit', $user->id) }}"
+                                                    class="text-blue-500 underline mr-2">{{ __('Editar') }}</a>
+                                                <form action="{{ route('profile.destroy', $user->id) }}" method="POST"
+                                                    class="inline">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit"
+                                                        class="text-red-500 underline">{{ __('Excluir') }}</button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                 </div>
             </div>
         </div>
-    </div>
-
-    <section>
-        <header>
-            <h2 class="text-lg font-medium text-gray-900">
-                {{ __('Gerenciar Usuários') }}
-            </h2>
-        </header>
-
-        <div class="mt-6">
-            <table class="min-w-full">
-                <thead>
-                    <tr>
-                        <th class="px-4 py-2">{{ __('Nome') }}</th>
-                        <th class="px-4 py-2">{{ __('E-mail') }}</th>
-                        <th class="px-4 py-2">{{ __('Unidade') }}</th>
-                        <th class="px-4 py-2">{{ __('Ações') }}</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($usuarios as $user)
-                        <tr>
-                            <td class="border px-4 py-2">{{ $user->name }}</td>
-                            <td class="border px-4 py-2">{{ $user->email }}</td>
-                            <td class="border px-4 py-2">{{ $user->unidade ?? 'Fatec Itapira - Ogari de Castro Pacheco' }}</td>
-                            <td class="border px-4 py-2">
-                                <a href="{{ route('profile.edit', $user->id) }}" class="text-blue-500 underline mr-2">{{ __('Editar') }}</a>
-                                <form action="{{ route('profile.destroy', $user->id) }}" method="POST" class="inline">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="text-red-500 underline">{{ __('Excluir') }}</button>
-                                </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-    </section>
+        </section>
 </x-app-layout>
